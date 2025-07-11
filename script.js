0b112820549b2db75b5295d6d1116f8e08a9a71a
Ashu463 <ashukasaudhan971@gmail.com>
2025-07-11 11:53:04 +0530

    asdf

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ const { owner, repo } = context.repo;
               console.log('Skipping check - PR is not being opened');
               return;
             }
-            // asf
             
             // Get the PR branch name
             const headBranch = pr.head.ref;
